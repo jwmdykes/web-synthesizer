d991b45ffdc54657eed69eb6045a31bdcff59462
ulazy1 <ulazy1@github.com>
2023-06-16 15:26:57 -0400

    add button to play sound (sound not implemented yet)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-full">
       <nav className="navbar bg-base-100">
         <div className="navbar-start">
           <a href="/" className='btn btn-ghost normal-case text-xl'>Synthesizer</a>
         </div>
       </nav>
+
+      <main className='h-full flex flex-col align-middle justify-center'>
+        <div className='flex justify-center'>
+          <button className='btn btn-primary'>Play Sound!</button>
+        </div>
+      </main>
     </div>
   );
 }
