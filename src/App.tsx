57cff46e5acecde3707172cbdfcd85f82fa8b67d
ulazy1 <ulazy1@github.com>
2023-06-16 15:40:15 -0400

    button working, playing sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function App() {
+  const audioContext = useRef<AudioContext | null>(null);
+
+  useEffect(() => {
+    const AudioContext = window.AudioContext;
+    audioContext.current = new AudioContext();
+  }, [])
+
+  const playSound = () => {
+    if (audioContext.current === null) {
+      return;
+    }
+
+    const oscillator = new OscillatorNode(audioContext.current, {
+      frequency: 440,
+      type: "sine"
+    })
+
+    oscillator.connect(audioContext.current.destination);
+    oscillator.start()
+  }
+
   return (
     <div className="App h-full">
       <nav className="navbar bg-base-100">
@@ -11,7 +32,7 @@ function App() {
 
       <main className='h-full flex flex-col align-middle justify-center'>
         <div className='flex justify-center'>
-          <button className='btn btn-primary'>Play Sound!</button>
+          <button className='btn btn-primary' onClick={() => playSound()}>Play Sound!</button>
         </div>
       </main>
     </div>
